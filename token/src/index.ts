b21edf2222eb362795a2319d4085f1723f25847c
adachi_440 <tomoki1999314@gmail.com>
2023-09-21 02:28:04 +0800

    first commit

---
+++ token/src/index.ts
@@ -0,0 +1,29 @@
+import {
+  AztecRPC,
+  Fr,
+  computeMessageSecretHash,
+  createAztecRpcClient,
+  createDebugLogger,
+  getSchnorrAccount,
+  waitForSandbox,
+} from '@aztec/aztec.js';
+import { GrumpkinScalar } from '@aztec/circuits.js';
+import { TokenContract } from '@aztec/noir-contracts/types';
+
+const { SANDBOX_URL = 'http://localhost:8080' } = process.env;
+
+async function main() {
+  ////////////// CREATE THE CLIENT INTERFACE AND CONTACT THE SANDBOX //////////////
+  const logger = createDebugLogger('token');
+
+  // We create AztecRPC client connected to the sandbox URL
+  const aztecRpc = createAztecRpcClient(SANDBOX_URL);
+  // Wait for sandbox to be ready
+  await waitForSandbox(aztecRpc);
+
+  const nodeInfo = await aztecRpc.getNodeInfo();
+
+  logger('Aztec Sandbox Info ', nodeInfo);
+}
+
+main();
